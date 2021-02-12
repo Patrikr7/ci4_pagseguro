e1aacaf62d59c0c1713444dad0270a2325ec1834
Emerson Patrik <epribeiro@clig.net.br>
2021-02-12 17:29:28 -0300

    Desenvolvimento em andamento.

--- app/Config/Constants/Constants.php
+++ app/Config/Constants/Constants.php
@@ -1,18 +1,18 @@
 <?php
 
 // BANCO DE DADOS
-defined('HOSTNAME') || define('HOSTNAME', 'seu_hostname');
-defined('USERNAME') || define('USERNAME', 'seu_username');
-defined('PASSWORD') || define('PASSWORD', 'seu_password');
-defined('DATABASE') || define('DATABASE', 'seu_database');
+defined('HOSTNAME') || define('HOSTNAME', 'localhost');
+defined('USERNAME') || define('USERNAME', 'root');
+defined('PASSWORD') || define('PASSWORD', 'senac');
+defined('DATABASE') || define('DATABASE', 'ci4_pagseguro');
 
 
 defined('BASE_URL') || define('BASE_URL', (!empty($_SERVER['HTTPS']) ? 'https://' . preg_replace('/index.php.*/', '', $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']) : 'http://' . preg_replace('/index.php.*/', '', $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'])));
 
 // DADOS PAGSEGURO
 defined('PAG_ENV') || define('PAG_ENV', 'sandbox');
-defined('PAG_EMAIL') || define('PAG_EMAIL', 'seu_email');
-defined('PAG_TOKEN') || define('PAG_TOKEN', 'seu_token');
+defined('PAG_EMAIL') || define('PAG_EMAIL', 'patrikr11@gmail.com');
+defined('PAG_TOKEN') || define('PAG_TOKEN', '4686A1B6830841E09B4BCD068EE301B3');
 defined('PAG_ID') || define('PAG_ID', '');
 defined('PAG_KEY') || define('PAG_KEY', '');
 defined('PAG_LOG') || define('PAG_LOG', WRITEPATH . "logs\pagseguro\log.log");
